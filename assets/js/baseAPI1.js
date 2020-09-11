95a687dff831ad08eb9528b5109a3eaea109884d
啦啦啦 <2431394501@qq.com>
2020-09-11 23:02:38 +0800

    主页基本完成

--- assets/js/baseAPI1.js
+++ assets/js/baseAPI1.js
@@ -1,3 +1,14 @@
 $.ajaxPrefilter(function (options) {
+    //统一配置url
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    //统一配置请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = { Authorization: localStorage.getItem('token') || '' }
+    };
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
--- assets/js/index1.js
+++ assets/js/index1.js
@@ -1,18 +1,26 @@
 $(function () {
-    function getUserInfo() {
-        $.ajax({
-            url: '/my/userinfo',
-            method: 'get',
-            headers: { Authorization: localStorage.getItem('token') },
-            success: function (res) {
-                console.log(res);
-                if (res.status !== 0) return layui.layer.msg('获取用户信息失败');
-                renderAvatar(res.data)
-            }
-        })
-    }
     getUserInfo()
+    var layer = layui.layer
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确认退出登录？', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+            getUserInfo()
+        })
+    })
+
 })
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        method: 'get',
+        success: function (res) {
+            if (res.status !== 0) return layui.layer.msg('获取用户信息失败');
+            renderAvatar(res.data)
+        }
+    })
+}
 function renderAvatar(user) {
     //
     var nickname = user.nickname
